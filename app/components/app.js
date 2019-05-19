d94d1b4e563eda2d5a9470f2ad20e1f607ecd15e
Munkhzorig Zurgaanjin <zorigoo@Munkhzorigs-MacBook-Pro.local>
2019-05-19 16:55:00 -0700

    Zorigoos first commit

---
+++ app/components/app.js
@@ -0,0 +1,23 @@
+var React = require('react');
+var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require ('./Nav');
+var Home = require ('./Home');
+
+class App extends React.Component {
+  render() {
+    return (
+    	<Router>
+      	<div className='container'>
+      		<Nav />
+      		<Route exact path='/' component = {Home}/>
+        	<Route path='/popular' component = {Popular}/>
+     			</div>
+      </Router>
+    )
+  }
+}
+
+module.exports = App;
